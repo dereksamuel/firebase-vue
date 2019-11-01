9e6fef5d6b4d02fdcbffff0b306a5ea818069167
dereksamuel <50559976+dereksamuel@users.noreply.github.com>
2019-10-31 22:34:11 -0500

    hola_Andy

--- src/views/Configuracion/index.vue
+++ src/views/Configuracion/index.vue
@@ -5,13 +5,32 @@
         Course Diary
       </span>
       <span class="areaname">
-        CONFIGURACIÓN
+        Configuración
       </span>
     </header>
+    <section class="form-wrap">
+      <section class="from-warp">
+        <a href="Usuarios.vue"><span class="fas fa-users-cog"></span></a>
+      </section>
+      <form action="" class="form_contact">
+        <div class="panel">
+        <div class="aj">
+          AJUSTES DE USUARIOS
+        </div>
+        <div class="aj">
+          Aquí puedes modificar tu estatus como usuario 
+          junto con el nombre de tal al hacer click en este título o en la imágen
+        </div>
+        </div>
+        <br>
+      </form>
+    <hr>
+    </section>
  </div>
 </template>
 <style scoped>
 @import url(https://fonts.googleapis.com/css?family=Bree+Serif|Courgette&display=swap);
+@import url(https://fonts.googleapis.com/css?family=Ubuntu&display=swap);
 
  .logo {
   font-size: 0.8em;
@@ -20,13 +39,51 @@
 .areaname {
   font-size: 1.3em;
   padding-left: 41%;
-  font-family:Courgette;
 }
 header {
   width: 100% auto;
   overflow: hidden;
   background: #ffe1b9;
 }
+.form-wrap .form_contact .panel{
+ width:auto;
+ margin-left:1.5vw;
+ background-color: aquamarine;
+ margin-right:1.5vw;
+}
+.form-wrap .form_contact .aj{
+  margin-top:2.6vh;
+ margin-left:2vw;
+ font-family:Ubuntu;
+ margin-right:2vw;
+}
+.form-wrap {
+  width: 70vw;
+  height: auto;
+  max-height: 1000px;
+  margin: 50px auto;
+  display: flex;
+  overflow: hidden;
+  margin-top: 5vh;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+  background: #cdf0f4;
+  margin-bottom: 35px;
+}
+.form-wrap span{
+  margin-left:5vw;
+  height:3vh;
+  font-size:30px;
+}
+.form-wrap .from-warp span{
+  margin-top:4vh;
+  margin-left:2vw;
+  border-radius:50%;
+  border-style:solid;
+  border-color: rgb(0, 0, 0);
+  height:5vh;
+  background-color:rgb(255, 255, 255);
+}
+
 body{
   background-color: aliceblue;
 }
